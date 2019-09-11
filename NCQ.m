603c9c96337367616e6618ca24c7daabc4418a01
Camille Carvalho <camillecarvalho@Camilles-Mac-mini.local>
2019-09-10 17:35:46 -0700

    edits files

--- NCQ.m
+++ NCQ.m
@@ -1,4 +1,4 @@
-function [M1,M2] =NCQ(Coorneu,Numtri,Reftri,Gd,G,Gds,Gs,mur_metal,mur_dielec,epsilonr_metal, epsilonr_dielec)
+function [M1,M2] =NCQ(Coorneu,Numtri,Reftri,Gd,G,Gds,Gs,mur_metal,mur_dielec,kw)
 % Compute the Newton-Cotes Quadrature (NCQ) over triangles.
 % The weights and nodes for second order NCQ can be found in "Symmetric
 % Quadrature Formulae for Simplexes", P. Silvester
@@ -21,62 +21,44 @@ nbq=length(weights_open);
 M1=zeros(ns,1);
 M2=zeros(ns,1);
 
-for t=1:nt
+for t=1:nt %loop over triangles
     
     %Elementary matrices
     S=[Coorneu(Numtri(t,1),:);Coorneu(Numtri(t,2),:);Coorneu(Numtri(t,3),:)];
     S21=S(2,:)-S(1,:);S31=S(3,:)-S(1,:);
     delta=S21(1)*S31(2)-S21(2)*S31(1);
-    Jflmt=[S31(2) -S21(2);-S31(1) S21(1)]/delta; %transfo affine!
     
     %Elementary matrices by quadratures
-    Mt1=zeros(q);Mt2=zeros(q); Mt11=zeros(q);Mt22=zeros(q);
-    for k=1:nbq
+    Mt1=zeros(q);Mt2=zeros(q);
+    
+    for k=1:nbq %loop over the triangle DOFs 
         x=pts_quadT(k,1);y=pts_quadT(k,2);
         
         if(q==3)  %P1
             w=[1-x-y x y];
-            gw=[-1 1 0;-1 0 1];
-        else % P2
-            w=[(1-x-y)*(1-2*x-2*y) x*(2*x-1) y*(2*y-1) 4*x*(1-x-y) 4*x*y 4*y*(1-x-y)]; 
-            gw=[4*(x+y)-3 4*x-1 0 4*(1-2*x-y) 4*y -4*y;
-                4*(x+y)-3 0 4*y-1 -4*x 4*x 4*(1-x-2*y)];
+        else      % P2
+            w=[(1-x-y)*(1-2*x-2*y) x*(2*x-1) y*(2*y-1) 4*x*(1-x-y) 4*x*y 4*y*(1-x-y)];
         end
- 
-        %coordonn�es dans le plan physique
-        P=S'*[1-x-y; x; y];
-
-        pk=weights_closed(k)*abs(delta);
         
-        if Reftri(t)==3 || Reftri(t)==4  || Reftri(t)==5 
-            Mt1=Mt1 +  pk * w' * Gd(k);
-            Mt11=Mt11 +  mur_metal * pk * w' * G(k);
-            Mt2=Mt2 +  pk * w' * Gds(k);
-            Mt22=Mt22 +  mur_metal * pk * w' * Gs(k);
-           % jg=Jflmt*gw;
-           % Kt=Kt+(1./epsilonr_metal) * pk * jg' * Gd(k); %gives a vector of size (6,1)
+        pk=weights_closed(k)*abs(delta); % weight by delta in physical domain
+        
+        if (Reftri(t)==3 || Reftri(t)==4  || Reftri(t)==5)
+            Mt1= Mt1 +  pk * w' * ( Gd(Numtri(t,k)) + kw^2 * mur_metal * G(Numtri(t,k)) );  
+            Mt2= Mt2 +  pk * w' * ( Gds(Numtri(t,k)) + kw^2 * mur_metal * Gs(Numtri(t,k)) );
         else
-            Mt1=Mt1 + pk * w' * Gd(k);
-            Mt11=Mt11 + mur_dielec * pk * w' * G(k);
-            Mt2=Mt2 + pk * w' * Gds(k);
-            Mt22=Mt22 + mur_dielec * pk * w' * Gs(k);
-           % jg=Jflmt*gw;
-           % Kt=Kt+(1./epsilonr_dielec) * pk * jg' * Gd(k);
+            Mt1= Mt1 + pk * w' * ( Gd(Numtri(t,k)) + kw^2 * mur_dielec * G(Numtri(t,k)) );
+            Mt2= Mt2 + pk * w' * ( Gds(Numtri(t,k))+ kw^2 * mur_dielec * Gs(Numtri(t,k)) );
+            
         end
-
+        
     end
-    
-    %assemblage de M et K
-   % I = Numtri(t,:)';
-   % M1(I) = M1(I)+ Mt1;
-   % M2(I) = M2(I) + Mt2;
     
     for i=1:q
         I=Numtri(t,i);
-        M1(I)=M1(I)+Mt1(i) + Mt11(i);
-        M2(I)=M2(I)+Mt2(i) + Mt22(i);
+        M1(I)=M1(I)+Mt1(i);
+        M2(I)=M2(I)+Mt2(i);
     end
     
-end  %fin boucle t
+end  
 end
 
--- singular_expression.m
+++ singular_expression.m
@@ -1,14 +1,18 @@
-function [Beta_s,div_s_grad_s, s_grad_s1,Zsingu,sPhi,Phi] = singular_expression_radial(eta, etap, etapp, Racine,phi,sigma_m,sigma_d)
-% Compute the radial componants o
-%
-%
-%
-%
+function [Beta_s,div_s_grad_s, s_grad_s1,Zsingu,sPhi,Phi] = singular_expression(eta, etap, etapp, Racine,phi,sigma_m,sigma_d)
+% Compute all analytical terms:
+% beta(zeta s zeta s)
+% div(sigma grad(s))
+% components of grad(s)
+% the singularity multilplied by the cutoff function: Zsingu  = eta * r^lambda * Phi
+% the weighted orthordial component of s: sigma * Phi
+% the orthordial component of s: Phi
+
+%1)Compute gradient parts (mostly radial components)
 div_s_grad_r  = @(r) (r*(1 + 2 * 1i * Racine) .* etap(r) + r.^2 .* etapp(r) - 2 * Racine^2 * eta(r)) .* r.^(1i * Racine -2);
 grad_r = @(r) (r.* etap(r) + 1i * Racine * eta(r)) .* r.^(1i * Racine -1);
 grad_t = @(r) eta(r) .* r.^(1i * Racine -1);
 
-
+%2) Define Phi, Phi', and sigma * Phi, sigma*Phi'
 Phi1  = @(t) sinh(Racine*(pi-t)) / sinh(Racine*(pi-phi/2));
 Phi2  = @(t) -sinh(Racine*(pi+t)) / sinh(Racine*(pi-phi/2));
 Phi3  = @(t) sinh(Racine*(t)) / sinh(Racine*(phi/2));
@@ -23,15 +27,16 @@ Phip3  = @(t) Racine*cosh(Racine*(t)) / sinh(Racine*(phi/2));
 [Phip] = @(t) (create_phip(Phip1, Phip2, Phip3, t, phi));
 [sPhip] = @(t) (create_sphip(Phip1, Phip2, Phip3, t, phi, sigma_m, sigma_d));
 
-%% Create expressions involving the singular part tobe computed via NCQ
+%3)Compute div(sigma grad(s)) and the gradients
 div_s_grad_s = @(r,t) sPhi(t).* div_s_grad_r(r); % TEST sans sigma
 s_grad_s1 = @(r,t) grad_r(r).* Phi(t) ;
 s_grad_s2 = @(r,t) grad_t(r).* Phip(t) ;
 
+%4) Compute Zsingu  = eta * r^lambda * Phi
 Zsingu = @(r,t) eta(r).* r.^(1i*Racine) .* Phi(t);
 
 %% Create exact terms involving the singular part only
-Beta_s  = 0;
+Beta_s  = 0; %TBD
 
 end
 
